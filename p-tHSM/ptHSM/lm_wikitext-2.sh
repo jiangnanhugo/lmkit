daf3d84f79c7ec0da5e2f84c17d6a429cc6ef8f1
Nan Jiang <jiangnan.hugo@gmail.com>
2017-04-21 21:18:10 +0800

    delete file

--- p-tHSM/ptHSM/lm_wikitext-2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python main.py --train_file ../data/wikitext-2/idx_wiki.train.tokens \
-            --valid_file ../data/wikitext-2/idx_wiki.valid.tokens \
-            --test_file ../data/wikitext-2/idx_wiki.test.tokens \
-            --vocab_size 33279
-            --batch_size 5
